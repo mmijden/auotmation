8986a5e6cc1e67933592875ae3c48bda1457192f
mmijden <101560287+mmijden@users.noreply.github.com>
2025-01-16 22:22:21 +0100

    Update testsamen.ps1

--- testsamen.ps1
+++ testsamen.ps1
@@ -6,35 +6,36 @@ $dccred = Import-Clixml -Path "C:\Users\admin\Documents\dc_credentials.xml"
 $dccred = New-Object System.Management.Automation.PSCredential ($dccred.UserName, $dccred.Password)
 Invoke-Command -ComputerName 10.3.0.2 -Credential $dccred -ScriptBlock {
     while ($true) {
-        $Response = Invoke-RestMethod http://10.3.0.40:8085/Staff
-        Write-Host $Response 
-        
-        foreach ($user in $Response.data) {
-            $voornaam = $user.Voornaam
-            $achternaam = $user.Achternaam
-            $personeelsnummer = $user.Personeelsnummer
-            $afdeling = $user.Afdeling
-            $email = $user.Email
+        try {
+            $Response = Invoke-RestMethod http://10.3.0.40:8085/Staff
+            Write-Host $Response 
+            
+            foreach ($user in $Response.data) {
+                $voornaam = $user.Voornaam
+                $achternaam = $user.Achternaam
+                $personeelsnummer = $user.Personeelsnummer
+                $afdeling = $user.Afdeling
+                $email = $user.Email
 
-            $samAccountName = "$voornaam $achternaam"
-            $wachtwoord = [System.Web.Security.Membership]::GeneratePassword(8,2)
-            $controlegebruiker = Get-ADUser -Filter {SamAccountName -eq $samAccountName}
-            if ($controlegebruiker -eq $null) {
-                New-ADUser -Name $voornaam -GivenName $voornaam -Surname $achternaam -SamAccountName $samAccountName -Department $afdeling -UserPrincipalName "$samAccountName@mijden.lan" -Path "OU=$afdeling,DC=mijden,DC=lan" -AccountPassword (ConvertTo-SecureString $wachtwoord -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $true
-                $object = [PSCustomObject]@{
-                    Voornaam = $voornaam
-                    Achternaam = $achternaam
-                    Afdeling = $afdeling
-                    Email = $email
-                    SamAccountName = $samAccountName
-                    Wachtwoord = $wachtwoord
-                }
-                $global:userRecords += $object
-                
-                $emailontvanger = $email
-                $emailafzender = "mike.fhict@gmail.com"
-                $onderwerp = "Uw nieuwe account"
-                $body = @"
+                $samAccountName = "$voornaam $achternaam"
+                $wachtwoord = [System.Web.Security.Membership]::GeneratePassword(8,2)
+                $controlegebruiker = Get-ADUser -Filter {SamAccountName -eq $samAccountName}
+                if ($controlegebruiker -eq $null) {
+                    New-ADUser -Name $voornaam -GivenName $voornaam -Surname $achternaam -SamAccountName $samAccountName -Department $afdeling -UserPrincipalName "$samAccountName@mijden.lan" -Path "OU=$afdeling,DC=mijden,DC=lan" -AccountPassword (ConvertTo-SecureString $wachtwoord -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $true
+                    $object = [PSCustomObject]@{
+                        Voornaam = $voornaam
+                        Achternaam = $achternaam
+                        Afdeling = $afdeling
+                        Email = $email
+                        SamAccountName = $samAccountName
+                        Wachtwoord = $wachtwoord
+                    }
+                    $global:userRecords += $object
+                    
+                    $emailontvanger = $email
+                    $emailafzender = "mike.fhict@gmail.com"
+                    $onderwerp = "Uw nieuwe account"
+                    $body = @"
 Beste $voornaam,
 Uw account is aangemaakt.
 Uw gebruikersnaam is: $samAccountName 
@@ -42,18 +43,21 @@ Uw wachtwoord is: $wachtwoord
 Met vriendelijke groet,
 IT-afdeling vd M
 "@
-                $smtpServer = "smtp.gmail.com"
-                $port= 587
-                $userName = 'mike.fhict@gmail.com'
-                $password = "lyzd gjuh otjy szdg"
-                [SecureString]$securepassword = $password | ConvertTo-SecureString -AsPlainText -Force
-                $credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $securepassword
+                    $smtpServer = "smtp.gmail.com"
+                    $port= 587
+                    $userName = 'mike.fhict@gmail.com'
+                    $password = "lyzd gjuh otjy szdg"
+                    [SecureString]$securepassword = $password | ConvertTo-SecureString -AsPlainText -Force
+                    $credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $securepassword
 
-                Send-MailMessage -To $emailontvanger -From $emailafzender -Subject $onderwerp -Body $body -SmtpServer $smtpServer -Port $port -UseSsl -Credential $credential
-                Write-Host "Gebruiker $samAccountName aangemaakt met wachtwoord: $wachtwoord er is een email verstuurd naar het opgegeven mail adress"
-            } else {
-                Write-Host "Gebruiker $samAccountName bestaat al."
+                    Send-MailMessage -To $emailontvanger -From $emailafzender -Subject $onderwerp -Body $body -SmtpServer $smtpServer -Port $port -UseSsl -Credential $credential
+                    Write-Host "Gebruiker $samAccountName aangemaakt met wachtwoord: $wachtwoord er is een email verstuurd naar het opgegeven mail adress"
+                } else {
+                    Write-Host "Gebruiker $samAccountName bestaat al."
+                }
             }
+        } catch {
+            Write-Host "Er is een fout opgetreden bij het verwerken van de gebruikers: $_"
         }
         Start-Sleep -Seconds 30
     }
@@ -81,79 +85,83 @@ while ($true) {
 
         Write-Host "Eerste gebruiker: Voornaam: $voornaam, Achternaam: $achternaam"
 
-        Connect-VIServer $vcenter -Credential $vcentercred
-        Start-Sleep -Seconds 60
+        try {
+            Connect-VIServer $vcenter -Credential $vcentercred
+            Start-Sleep -Seconds 60
 
-        $vmnaam = "ws-$voornaam$achternaam"
-        $hostname = "ws-$voornaam$achternaam"
-        $serverNaamOfIp = "test"
-        $gebruikerdc = $dccred.UserName
-        $domein = "mijden.lan"
-        $ip = "10.2.0.32"
-        $credential = New-Object System.Management.Automation.PSCredential ("admin", $wscred.Password)
+            $vmnaam = "ws-$voornaam$achternaam"
+            $hostname = "ws-$voornaam$achternaam"
+            $serverNaamOfIp = "test"
+            $gebruikerdc = $dccred.UserName
+            $domein = "mijden.lan"
+            $ip = "10.2.0.32"
+            $credential = New-Object System.Management.Automation.PSCredential ("admin", $wscred.Password)
 
-        Write-host $credential.Password
-        $vm = New-VM -name $vmnaam -Template ws-user-temp -ResourcePool I540703 -Datastore NIM01-1 -Location I540703  
+            Write-host $credential.Password
+            $vm = New-VM -name $vmnaam -Template ws-user-temp -ResourcePool I540703 -Datastore NIM01-1 -Location I540703  
 
-        Write-host $vmnaam
+            Write-Host "VM $vmnaam aangemaakt"
 
-        Start-Sleep -Seconds 60 
+            Start-Sleep -Seconds 60 
 
-        Start-VM -VM $vmnaam
+            Start-VM -VM $vmnaam
 
-        Start-Sleep -Seconds 60
+            Start-Sleep -Seconds 60
 
-        $vmTeZoeken = "ws-$voornaam$achternaam"
+            $vmTeZoeken = "ws-$voornaam$achternaam"
 
-        $vm = Get-VM -Name $vmTeZoeken
+            $vm = Get-VM -Name $vmTeZoeken
 
-        $ipv4Adres = $vm.Guest.IPAddress | Select-Object -First 1
-        Write-Output $ipv4Adres
+            $ipv4Adres = $vm.Guest.IPAddress | Select-Object -First 1
+            Write-Output $ipv4Adres
 
-        Start-Sleep -Seconds 90
-        Disconnect-VIServer -Server "vcenter.netlab.fontysict.nl" -Confirm:$false
+            Start-Sleep -Seconds 90
+            Disconnect-VIServer -Server "vcenter.netlab.fontysict.nl" -Confirm:$false
 
-        Invoke-Command -ComputerName $ipv4Adres -Credential $credential -ScriptBlock {
-            if ($using:eersteGebruiker.afdeling -eq "Hr") {  
-                winget install Google.Chrome --accept-package-agreements --accept-source-agreements --silent
-                start-sleep -seconds 30
-                Rename-Computer -NewName $using:hostname -Force -Restart
-            } else {
-                winget search Discord
-                winget install Discord -s msstore --accept-package-agreements --accept-source-agreements --silent
-                start-sleep -seconds 30
-                Rename-Computer -NewName $using:hostname -Force -Restart
+            Invoke-Command -ComputerName $ipv4Adres -Credential $credential -ScriptBlock {
+                if ($using:eersteGebruiker.afdeling -eq "Hr") {  
+                    winget install Google.Chrome --accept-package-agreements --accept-source-agreements --silent
+                    start-sleep -seconds 30
+                    Rename-Computer -NewName $using:hostname -Force -Restart
+                } else {
+                    winget search Discord
+                    winget install Discord -s msstore --accept-package-agreements --accept-source-agreements --silent
+                    start-sleep -seconds 30
+                    Rename-Computer -NewName $using:hostname -Force -Restart
+                }
             }
-        }
 
-        do {
-            Start-Sleep -Seconds 240
-            $pingResult = Test-Connection -ComputerName $ipv4Adres -Count 1 -Quiet
-        } until ($pingResult)
+            do {
+                Start-Sleep -Seconds 240
+                $pingResult = Test-Connection -ComputerName $ipv4Adres -Count 1 -Quiet
+            } until ($pingResult)
 
-        Start-Sleep -Seconds 120  # Wacht nu 120 seconden voordat je de computer aan het domein toevoegt
+            Start-Sleep -Seconds 120  # Wacht nu 120 seconden voordat je de computer aan het domein toevoegt
 
-        # Probeer de computer aan het domein toe te voegen met foutafhandeling
-        $domeinToevoegen = Invoke-Command -ComputerName $ipv4Adres -Credential $credential -ScriptBlock {
-            try {
-                Add-Computer -DomainName $using:domein -Credential $using:dccred -Force -Restart
-                return $true
-            } catch {
-                Write-Host "Fout bij het toevoegen aan het domein: $_"
-                return $false
+            # Probeer de computer aan het domein toe te voegen met foutafhandeling
+            $domeinToevoegen = Invoke-Command -ComputerName $ipv4Adres -Credential $credential -ScriptBlock {
+                try {
+                    Add-Computer -DomainName $using:domein -Credential $using:dccred -Force -Restart
+                    return $true
+                } catch {
+                    Write-Host "Fout bij het toevoegen aan het domein: $_"
+                    return $false
+                }
             }
+
+            if (-not $domeinToevoegen) {
+                Write-Host "Domein join mislukt. Probeer opnieuw."
+                Start-Sleep -Seconds 30
+                continue  # Ga door met de volgende iteratie van de while-lus
+            }
+
+            # Verwijder de eerste gebruiker uit de array na succesvolle domeinjoin
+            $global:userRecords = $global:userRecords | Where-Object { $_ -ne $eersteGebruiker }
+
+            Write-Host "Eerste gebruiker succesvol verwerkt en verwijderd uit de array."
+        } catch {
+            Write-Host "Er is een fout opgetreden bij het aanmaken van de VM: $_"
         }
-
-        if (-not $domeinToevoegen) {
-            Write-Host "Domein join mislukt. Probeer opnieuw."
-            Start-Sleep -Seconds 30
-            continue  # Ga door met de volgende iteratie van de while-lus
-        }
-
-        # Verwijder de eerste gebruiker uit de array na succesvolle domeinjoin
-        $global:userRecords = $global:userRecords | Where-Object { $_ -ne $eersteGebruiker }
-
-        Write-Host "Eerste gebruiker succesvol verwerkt en verwijderd uit de array."
     } else {
         Write-Host "Geen gebruikers gevonden in de array. Wacht op nieuwe invoer."
         Start-Sleep -Seconds 60

c34037a59cf3332288f7c10bcb4d074996720abc
mmijden <101560287+mmijden@users.noreply.github.com>
2025-01-17 23:24:01 +0100

    Add files via upload

--- alles/onboarding_script.ps1
+++ alles/onboarding_script.ps1
@@ -1,7 +1,10 @@
+# Define credentials and vCenter information
 $dccred = Import-Clixml -Path "C:\Users\admin\Documents\dc_credentials.xml"
-$dccred = New-Object System.Management.Automation.PSCredential ($dccred.UserName, $dccred.Password)
+$vcentercred = Import-Clixml -Path "C:\Users\admin\Documents\vcenter_credentials.xml"
+$wscred = Import-Clixml -Path "C:\Users\admin\Documents\ws_credentials.xml"
+$vcenter = "vcenter.netlab.fontysict.nl"
+
 Invoke-Command -ComputerName 10.3.0.2 -Credential $dccred -ScriptBlock {
-    $bestaandeGebruikers = @()
     while ($true) {
         $Response = Invoke-RestMethod http://10.3.0.40:8085/Staff
         Write-Host $Response 
@@ -9,96 +12,75 @@ Invoke-Command -ComputerName 10.3.0.2 -Credential $dccred -ScriptBlock {
         foreach ($user in $response.data) {
             $voornaam = $user.Voornaam
             $achternaam = $user.Achternaam
-            $personeelsnummer = $user.Personeelsnummer
             $afdeling = $user.Afdeling
             $email = $user.Email
             
-
             $samAccountName = "$voornaam $achternaam"
             $wachtwoord = [System.Web.Security.Membership]::GeneratePassword(8,2)
             $controlegebruiker = Get-ADUser -Filter {SamAccountName -eq $samAccountName}
             if ($controlegebruiker -eq $null) {
                 New-ADUser -Name $voornaam -GivenName $voornaam -Surname $achternaam -SamAccountName $samAccountName -Department $afdeling -UserPrincipalName "$samAccountName@mijden.lan" -Path "OU=$afdeling,DC=mijden,DC=lan" -AccountPassword (ConvertTo-SecureString $wachtwoord -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $true
-                $object = [PSCustomObject]@{
-                Voornaam = $voornaam
-                Achternaam = $achternaam
-                Afdeling = $afdeling
-                }
+                
                 $emailontvanger = $email
                 $emailafzender = "mike.fhict@gmail.com"
                 $onderwerp = "Uw nieuwe account"
-                $body = @"
-Beste $voornaam,
-Uw account is aangemaakt.
-Uw gebruikersnaam is: $samAccountName 
-Uw wachtwoord is: $wachtwoord
-Met vriendelijke groet,
-IT-afdeling vd M
-"@
+                $body = "Beste $voornaam, Uw account is aangemaakt. Uw gebruikersnaam is: $samAccountName. Uw wachtwoord is: $wachtwoord."
                 $smtpServer = "smtp.gmail.com"
                 $port= 587
                 $userName = 'mike.fhict@gmail.com'
                 $password = "lyzd gjuh otjy szdg"
                 [SecureString]$securepassword = $password | ConvertTo-SecureString -AsPlainText -Force
-                $credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $securepassword
-
+                $credential = New-Object System.Management.Automation.PSCredential -ArgumentList $userName, $securepassword
+                
                 Send-MailMessage -To $emailontvanger -From $emailafzender -Subject $onderwerp -Body $body -SmtpServer $smtpServer -Port $port -UseSsl -Credential $credential
-                Write-Host "Gebruiker $samAccountName aangemaakt met wachtwoord: $wachtwoord er is een email verstuurd naar het opgegeven mail adress"
+                Write-Host "Gebruiker $samAccountName aangemaakt met wachtwoord: $wachtwoord. Er is een email verstuurd naar het opgegeven mailadres."
+
+                Connect-VIServer $vcenter -Credential $vcentercred
+                Start-Sleep -Seconds 60
 
                 $vmnaam = "ws-$voornaam$achternaam"
                 $hostname = "ws-$voornaam$achternaam"
-                $serverNaamOfIp = "test"
-                $gebruikerdc = $dccred.UserName
-                $domein = "mijden.lan"
-                $ip = "10.2.0.32"
                 $credential = New-Object System.Management.Automation.PSCredential ("admin", $dccred.Password)
 
-                Write-host $credential.Password
                 $vm = New-VM -name $vmnaam -Template ws-user-temp -ResourcePool I540703 -Datastore NIM01-1 -Location I540703  
-
                 Write-Host "VM $vmnaam aangemaakt"
-
                 Start-Sleep -Seconds 60 
 
                 Start-VM -VM $vmnaam
-
                 Start-Sleep -Seconds 60
 
-                $vmTeZoeken = "ws-$voornaam$achternaam"
-
-                $vm = Get-VM -Name $vmTeZoeken
-
+                $vm = Get-VM -Name $vmnaam
                 $ipv4Adres = $vm.Guest.IPAddress | Select-Object -First 1
-                Write-Output $ipv4Adres
 
-                Start-Sleep -Seconds 90
-                Disconnect-VIServer -Server "vcenter.netlab.fontysict.nl" -Confirm:$false
+                Disconnect-VIServer -Server $vcenter -Force -Confirm:$false
+
+                Invoke-Command -ComputerName $ipv4Adres -Credential $wscred -ScriptBlock {
+                    Rename-Computer -NewName $using:hostname -Force -Restart
+                }
+                
+                Start-Sleep -Seconds 90  
+
+                $vm = Get-VM -Name $vmnaam
+
+             
+                while ($vm.PowerState -ne 'PoweredOn') {
+                    Start-Sleep -Seconds 10
+                    $vm = Get-VM -Name $vmnaam
+                }
 
                 Invoke-Command -ComputerName $ipv4Adres -Credential $credential -ScriptBlock {
-                    if ($using:afdeling -eq "Hr") {  
-                        winget install Google.Chrome --accept-package-agreements --accept-source-agreements --silent
-                        start-sleep -seconds 30
-                        Rename-Computer -NewName $using:hostname -Force -Restart
-                    } else {
-                        winget search Discord
+                    if ($using:afdeling -eq "Hr") {
                         winget install Discord -s msstore --accept-package-agreements --accept-source-agreements --silent
-                        start-sleep -seconds 30
-                        Rename-Computer -NewName $using:hostname -Force -Restart
+                    } else {
+                        winget install Google.Chrome --accept-package-agreements --accept-source-agreements --silent
                     }
+                    start-sleep -seconds 30
+                    Add-Computer -DomainName "mijden.lan" -Credential $using:credential -Force -Restart
                 }
             } else {
-                
-                
-                if (-not $bestaandeGebruikers.Contains($samAccountName)) {
-                    Write-Host "Gebruiker $samAccountName bestaat al."
-                    $bestaandeGebruikers += $samAccountName  
-                }
-                
-
+                Write-Host "Gebruiker $samAccountName bestaat al."
             }
         }
         Start-Sleep -Seconds 30
-
     }
-    
 }

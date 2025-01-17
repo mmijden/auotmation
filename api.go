4f1d64bd678c0ec7b51789bcbefbde7281d6ef22
mmijden <101560287+mmijden@users.noreply.github.com>
2025-01-17 19:11:18 +0100

    Add files via upload

--- api.go
+++ api.go
@@ -14,21 +14,10 @@ type FormData struct {
 }
 
 var storedData FormData
+var lastOffboardRequest map[string]string
 
 func main() {
 	r := gin.Default()
-
-	// Enable CORS
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-		c.Next()
-	})
 
 	r.POST("/submit", func(c *gin.Context) {
 		var formData FormData
@@ -47,10 +36,22 @@ func main() {
 	r.POST("/offboard", func(c *gin.Context) {
 		voornaam := c.PostForm("Voornaam")
 		achternaam := c.PostForm("Achternaam")
-		c.JSON(http.StatusOK, gin.H{"message": "Offboarding gestart", "data": gin.H{
+		
+		lastOffboardRequest = map[string]string{
 			"Voornaam": voornaam,
 			"Achternaam": achternaam,
-		}})
+		}
+		
+		c.JSON(http.StatusOK, gin.H{"message": "Offboarding gestart"})
+	})
+
+	r.GET("/offboard", func(c *gin.Context) {
+		if lastOffboardRequest != nil {
+			c.JSON(http.StatusOK, gin.H{"data": lastOffboardRequest})
+			lastOffboardRequest = nil
+		} else {
+			c.JSON(http.StatusOK, gin.H{"data": map[string]string{}})
+		}
 	})
 
 	r.Run(":8085")

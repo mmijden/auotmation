268450af3e98edf5bc41d8917b327960bb86cdbb
mmijden <101560287+mmijden@users.noreply.github.com>
2025-01-17 19:17:23 +0100

    Add files via upload

--- manual_offboarding.ps1
+++ manual_offboarding.ps1
@@ -3,44 +3,33 @@ $vcenter = "vcenter.netlab.fontysict.nl"
 $vcentercred = Import-Clixml -Path "C:\Users\admin\Documents\vcenter_credentials.xml"
 $dccred = Import-Clixml -Path "C:\Users\admin\Documents\dc_credentials.xml"
 
-# Configure PowerCLI
-Set-PowerCLIConfiguration -Scope AllUsers -InvalidCertificateAction Warn
-
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$Voornaam,
+while ($true) {
+    $Response = Invoke-RestMethod http://10.3.0.40:8085/offboard
     
-    [Parameter(Mandatory=$true)]
-    [string]$Achternaam
-)
+    if ($Response.data.Voornaam) {
+        $voornaam = $Response.data.Voornaam
+        $achternaam = $Response.data.Achternaam
+        
+        Write-Host "Start offboarding voor $voornaam $achternaam"
+        
+        # VM uitzetten en verwijderen
+        Connect-VIServer $vcenter -Credential $vcentercred
+        $vm = Get-VM -Name "ws-$voornaam-$achternaam" -ErrorAction SilentlyContinue
+        if ($vm) {
+            Stop-VM -VM $vm -Confirm:$false
+            Remove-VM -VM $vm -DeletePermanently -Confirm:$false
+        }
+        Disconnect-VIServer -Server $vcenter -Confirm:$false
 
-Write-Host "Start offboarding voor $Voornaam $Achternaam..."
-
-# Controleer of gebruiker bestaat
-$samAccountName = "$Voornaam $Achternaam"
-$gebruiker = Get-ADUser -Filter {SamAccountName -eq $samAccountName}
-
-if ($gebruiker) {
-    # 1. VM uitzetten en verwijderen
-    Connect-VIServer $vcenter -Credential $vcentercred
-    $vmnaam = "ws-$Voornaam-$Achternaam"
-    
-    $vm = Get-VM -Name $vmnaam -ErrorAction SilentlyContinue
-    if ($vm) {
-        Stop-VM -VM $vm -Confirm:$false
-        Remove-VM -VM $vm -DeletePermanently -Confirm:$false
-        Write-Host "VM $vmnaam is verwijderd"
+        # AD account uitschakelen en verplaatsen
+        $gebruiker = Get-ADUser -Filter {SamAccountName -eq "$voornaam $achternaam"}
+        if ($gebruiker) {
+            Disable-ADAccount -Identity $gebruiker
+            Move-ADObject -Identity $gebruiker.DistinguishedName -TargetPath "OU=Disabled Users,DC=mijden,DC=lan"
+        }
+        
+        Write-Host "Klaar met $voornaam $achternaam"
     }
     
-    Disconnect-VIServer -Server $vcenter -Confirm:$false
-
-    # 2. AD account uitschakelen en verplaatsen
-    Disable-ADAccount -Identity $gebruiker
-    Move-ADObject -Identity $gebruiker.DistinguishedName -TargetPath "OU=Disabled Users,DC=mijden,DC=lan"
-    Write-Host "AD account $samAccountName is uitgeschakeld en verplaatst"
-
-    Write-Host "Offboarding voltooid voor $samAccountName"
-}
-else {
-    Write-Host "Gebruiker $samAccountName niet gevonden"
+    Start-Sleep -Seconds 30
 }

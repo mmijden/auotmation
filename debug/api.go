3f77266ee28abdde780f6b66bf4f73107fff1476
mmijden <101560287+mmijden@users.noreply.github.com>
2025-01-18 19:58:03 +0100

    Create api.go

---
+++ debug/api.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+type FormData struct {
+	Personeelsnummer string `form:"Personeelsnummer" binding:"required"`
+	Voornaam         string `form:"Voornaam" binding:"required"`
+	Achternaam       string `form:"Achternaam" binding:"required"`
+	Afdeling         string `form:"Afdeling" binding:"required"`
+	Email            string `form:"Email" binding:"required"`
+}
+
+var storedData FormData
+var lastOffboardRequest map[string]string
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/submit", func(c *gin.Context) {
+		var formData FormData
+		if err := c.ShouldBind(&formData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		storedData = formData
+		c.JSON(http.StatusOK, gin.H{"message": "Gegevens ontvangen", "data": formData})
+	})
+
+	r.GET("/Staff", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": storedData})
+	})
+
+	r.POST("/offboard", func(c *gin.Context) {
+		Voornaam := c.PostForm("Voornaam")
+		Achternaam := c.PostForm("Achternaam")
+
+		fmt.Println("Ontvangen Voornaam:", Voornaam)
+    		fmt.Println("Ontvangen Achternaam:", Achternaam)
+		
+		if Voornaam == "" || Achternaam == "" {
+        	fmt.Println("Waarschuwing: Geen gegevens ontvangen voor Voornaam of Achternaam!")
+    		}
+
+
+		lastOffboardRequest = map[string]string{
+			"Voornaam": Voornaam,
+			"Achternaam": Achternaam,
+		}
+		
+		c.JSON(http.StatusOK, gin.H{"message": "Offboarding gestart"})
+	})
+
+	r.GET("/offboard", func(c *gin.Context) {
+		if lastOffboardRequest != nil {
+			c.JSON(http.StatusOK, gin.H{"data": lastOffboardRequest})
+			lastOffboardRequest = nil
+		} else {
+			c.JSON(http.StatusOK, gin.H{"data": map[string]string{}})
+		}
+	})
+
+	r.Run(":8085")
+}

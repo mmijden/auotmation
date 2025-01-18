dc87ca3aa26b68fb3a3f3543b1fe2ecabbb2deeb
mmijden <101560287+mmijden@users.noreply.github.com>
2025-01-18 16:08:07 +0100

    Add files via upload

--- alles/create_vm_api_automatisch.ps1
+++ alles/create_vm_api_automatisch.ps1
@@ -24,91 +24,85 @@ while ($true) {
                 $email = $user.email
                 $afdeling = $user.afdeling
 
-                if (-not ($processedUsers -contains "$voornaam $achternaam")) {
-                    Write-Host "gebruiker: Voornaam: $voornaam, Achternaam: $achternaam, Email: $email, Afdeling: $afdeling"
-                    Connect-VIServer $vcenter -Credential $vcentercred
-                    Start-Sleep -Seconds 60
+                $vmnaam = "ws-$voornaam$achternaam"
+                $existingVM = Get-VM -Name $vmnaam -ErrorAction SilentlyContinue
+                $existingUser = Get-ADUser -Identity "$voornaam $achternaam" -ErrorAction SilentlyContinue
 
-                    $vmnaam = "ws-$voornaam$achternaam"
-
-                    $existingVM = Get-VM -Name $vmnaam -ErrorAction SilentlyContinue
-                    if ($existingVM) {
-                        Write-Host "VM $vmnaam bestaat al. Overslaan."
-                        Disconnect-VIServer -Server $vcenter -Confirm:$false
-                        Start-Sleep -Seconds 30
-                        continue
-                    }
-
-                    $existingUser = Get-ADUser -Identity "$voornaam $achternaam" -ErrorAction SilentlyContinue
-                    if ($existingUser) {
-                        Write-Host "Gebruiker $voornaam $achternaam bestaat al. Overslaan."
-                        Disconnect-VIServer -Server $vcenter -Confirm:$false
-                        Start-Sleep -Seconds 30
-                        continue
-                    }
-
-                    $hostname = "ws-$voornaam$achternaam"
-                    $wachtwoord = "Mike123"
-                    $serverNaamOfIp = "test"
-                    $gebruikerdc = $dccred.UserName
-                    $domein = "mijden.lan"
-                    $ip = "10.2.0.32"
-                    $credential = New-Object System.Management.Automation.PSCredential ("admin", $wscred.Password)
-
-                    Write-host $credential.Password
-                    $vm = New-VM -name $vmnaam -Template ws-user-temp -ResourcePool I540703 -Datastore NIM01-1 -Location I540703
-                    Write-host $vmnaam
-                    Start-Sleep -Seconds 60
-                    Start-VM -VM $vmnaam
-                    Start-Sleep -Seconds 60
-
-                    $vmTeZoeken = "ws-$voornaam$achternaam"
-                    $vm = Get-VM -Name $vmTeZoeken
-                    $ipv4Adres = $vm.Guest.IPAddress | Select-Object -First 1
-                    Write-Output $ipv4Adres
-                    Start-Sleep -Seconds 90
-                    Disconnect-VIServer -Server "vcenter.netlab.fontysict.nl" -Confirm:$false
-
-                    Invoke-Command -ComputerName $ipv4Adres -Credential $credential -ScriptBlock {
-                        if ($using:afdeling -eq "Hr") {
-                            winget install Google.Chrome --accept-package-agreements --accept-source-agreements --silent
-                            start-sleep -seconds 30
-                            Rename-Computer -NewName $using:hostname -Force -Restart
-                        } else {
-                            winget search Discord
-                            winget install Discord -s msstore --accept-package-agreements --accept-source-agreements --silent
-                            Start-Sleep -seconds 30
-                            Rename-Computer -NewName $using:hostname -Force -Restart
-                        }
-                    }
-
-                    do {
-                        Start-Sleep -Seconds 240
-                        $pingResult = Test-Connection -ComputerName $ipv4Adres -Count 1 -Quiet
-                    } until ($pingResult)
-
-                    Start-Sleep -Seconds 120  
-
-                    $domeinToevoegen = Invoke-Command -ComputerName $ipv4Adres -Credential $credential -ScriptBlock {
-                        try {
-                            Add-Computer -DomainName $using:domein -Credential $using:dccred -Force -Restart
-                            return $true
-                        } catch {
-                            Write-Host "Fout bij het toevoegen aan het domein: $_"
-                            return $false
-                        }
-                    }
-
-                    if (-not $domeinToevoegen) {
-                        Write-Host "Domein join mislukt. Probeer opnieuw."
-                        Start-Sleep -Seconds 30
-                    }
-
-                    Write-Host "Eerste gebruiker succesvol verwerkt."
-                    $processedUsers += "$voornaam $achternaam" 
-                } else {
-                    Write-Host "Gebruiker $voornaam $achternaam bestaat al."
+                if ($existingUser) {
+                    Write-Host "Gebruiker $voornaam $achternaam bestaat al. Overslaan."
+                    Start-Sleep -Seconds 30
+                    continue
                 }
+
+                if ($existingVM) {
+                    Write-Host "VM $vmnaam bestaat al. Overslaan."
+                    Start-Sleep -Seconds 30
+                    continue
+                }
+
+                Write-Host "VM en gebruiker voor $voornaam $achternaam worden aangemaakt."
+                Connect-VIServer $vcenter -Credential $vcentercred
+                Start-Sleep -Seconds 60
+
+                $hostname = "ws-$voornaam$achternaam"
+                $wachtwoord = "Mike123"
+                $serverNaamOfIp = "test"
+                $gebruikerdc = $dccred.UserName
+                $domein = "mijden.lan"
+                $ip = "10.2.0.32"
+                $credential = New-Object System.Management.Automation.PSCredential ("admin", $wscred.Password)
+
+                Write-host $credential.Password
+                $vm = New-VM -name $vmnaam -Template ws-user-temp -ResourcePool I540703 -Datastore NIM01-1 -Location I540703
+                Write-host $vmnaam
+                Start-Sleep -Seconds 60
+                Start-VM -VM $vmnaam
+                Start-Sleep -Seconds 60
+
+                $vmTeZoeken = "ws-$voornaam$achternaam"
+                $vm = Get-VM -Name $vmTeZoeken
+                $ipv4Adres = $vm.Guest.IPAddress | Select-Object -First 1
+                Write-Output $ipv4Adres
+                Start-Sleep -Seconds 90
+                Disconnect-VIServer -Server "vcenter.netlab.fontysict.nl" -Confirm:$false
+
+                Invoke-Command -ComputerName $ipv4Adres -Credential $credential -ScriptBlock {
+                    if ($using:afdeling -eq "Hr") {
+                        winget install Google.Chrome --accept-package-agreements --accept-source-agreements --silent
+                        Start-Sleep -seconds 30
+                        Rename-Computer -NewName $using:hostname -Force -Restart
+                    } else {
+                        winget search Discord
+                        winget install Discord -s msstore --accept-package-agreements --accept-source-agreements --silent
+                        Start-Sleep -seconds 30
+                        Rename-Computer -NewName $using:hostname -Force -Restart
+                    }
+                }
+
+                do {
+                    Start-Sleep -Seconds 240
+                    $pingResult = Test-Connection -ComputerName $ipv4Adres -Count 1 -Quiet
+                } until ($pingResult)
+
+                Start-Sleep -Seconds 120
+
+                $domeinToevoegen = Invoke-Command -ComputerName $ipv4Adres -Credential $credential -ScriptBlock {
+                    try {
+                        Add-Computer -DomainName $using:domein -Credential $using:dccred -Force -Restart
+                        return $true
+                    } catch {
+                        Write-Host "Fout bij het toevoegen aan het domein: $_"
+                        return $false
+                    }
+                }
+
+                if (-not $domeinToevoegen) {
+                    Write-Host "Domein join mislukt. Probeer opnieuw."
+                    Start-Sleep -Seconds 30
+                }
+
+                Write-Host "Eerste gebruiker succesvol verwerkt."
+                $processedUsers += "$voornaam $achternaam" 
             } else {
                 Write-Host 'Geen gegevens ontvangen, wacht op gegevens'
                 Start-Sleep -Seconds 30
